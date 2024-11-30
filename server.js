c4ff1b7a5e9336827645b596f6cb9f207bff3cb8
khoi tran <toptoplan10@gmail.com>
2024-11-30 14:33:41 +0700

    Thêm API key vào header

--- server.js
+++ server.js
@@ -15,12 +15,11 @@ app.use('/proxy', createProxyMiddleware({
     '^/proxy': '',  // Xóa '/proxy' khỏi đường dẫn khi chuyển tiếp
   },
   onProxyReq: (proxyReq, req, res) => {
-    // Chuyển tiếp yêu cầu và giữ nguyên API key trong URL
+    // Thêm API key vào header yêu cầu
     const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpeGNseWFnbnhmaHB4bnBkaHF1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMjM3NzE3MywiZXhwIjoyMDQ3OTUzMTczfQ.NuH6Ith4qM-LUEHVbska9EbRw6iVwgYFKl-Gdg1sgwA'; // API key thực tế
     
-    // Thêm API key vào đường dẫn của yêu cầu
-    const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}?apikey=${apiKey}`;
-    proxyReq.url = fullUrl;
+    // Thêm API key vào header của yêu cầu
+    proxyReq.setHeader('apikey', apiKey);
   }
 }));
 
