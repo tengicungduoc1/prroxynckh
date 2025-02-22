62f765e93fe2743ae1365e6363443d4b89a2df56
khoi tran <toptoplan10@gmail.com>
2025-02-23 00:23:27 +0700

    First commiAt

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const moment = require('moment-timezone');
 
 const app = express();
 
-// Áp dụng express.json() cho tất cả các route trước khi đến proxy
+// Áp dụng express.json() và express.urlencoded() cho tất cả các route
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // URL của Supabase REST API
 const SUPABASE_URL = 'https://hyctwifnimvyeirdwzsb.supabase.co/rest/v1';
 
-// Cấu hình proxy
 app.use(
   '/proxy',
   createProxyMiddleware({
     target: SUPABASE_URL,
     changeOrigin: true,
-    secure: false,
+    secure: false, // Đặt thành true nếu sử dụng HTTPS với chứng chỉ hợp lệ
     timeout: 120000,
     proxyTimeout: 120000,
     pathRewrite: {
@@ -26,19 +25,20 @@ app.use(
     onProxyReq: (proxyReq, req, res) => {
       console.log(`Proxying ${req.method} request to ${proxyReq.path}`);
 
-      if (['POST', 'PUT', 'PATCH'].includes(req.method) && req.body) {
+      if (
+        ['POST', 'PUT', 'PATCH'].includes(req.method.toUpperCase()) &&
+        req.body
+      ) {
+        // Chuyển đổi body thành chuỗi JSON
         const bodyData = JSON.stringify(req.body);
 
-        // Đặt lại header Content-Length và Content-Type
-        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // Cập nhật header 'Content-Type' và 'Content-Length'
         proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
 
-        // Ghi dữ liệu body vào request proxy
+        // Ghi dữ liệu body vào proxyReq
         proxyReq.write(bodyData);
       }
-
-      // Xoá header 'Transfer-Encoding' nếu có
-      proxyReq.removeHeader('Transfer-Encoding');
     },
     onProxyRes: (proxyRes, req, res) => {
       console.log(`Received response with status code: ${proxyRes.statusCode}`);
@@ -50,7 +50,7 @@ app.use(
   })
 );
 
-// Endpoint kiểm tra server
+// Endpoint kiểm tra server: lấy thời gian theo múi giờ Việt Nam
 app.get('/time', (req, res) => {
   const currentTime = moment()
     .tz('Asia/Ho_Chi_Minh')
